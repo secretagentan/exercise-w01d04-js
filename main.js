5757fff8454f24fbf601dfa93826ee3d14dc602c
Phil Lamplugh <phillip.lamplugh@gmail.com>
2016-12-01 12:38:51 -0800

    basic exercise

---
+++ main.js
@@ -0,0 +1,38 @@
+// task 1
+// Write a function called knighted that takes a name and returns it with "Sir" in front
+// when you use it - it should look like this
+
+// var name = knighted('Elton');
+// console.log(name) => 'Sir Elton'
+
+// task 2
+// Write a function called doubleIt that takes a number, doubles it and returns the new number
+
+// var answer = doubleIt(2);
+// console.log(answer) => 4
+
+// task 3
+// Write a function called makeUser that takes a name and returns a new object
+
+// var user = makeUser('Dave');
+// console.log(user) => {name: 'Dave'}
+
+// task 4
+// Write a function called makeLink that takes a title and a url and returns a string of html
+
+// var aTag = makeLink('google', 'http://www.google.com');
+// console.log(aTag) => '<a href="http://www.google.com">google</a>'
+
+// task 5
+// Write a function called second that takes an array and returns the 2nd element in it
+
+// var answer = second(['a', 'b', 'c']);
+// console.log(answer) => 'b'
+
+// task 6
+// Write a function called toSentence that takes an array and returns a string
+// where the items are joined by commas (except the last one which is joined with 'and')
+// where the first letter is capitalized and the string ends in an exclamation point.
+
+// var sentence = toSentence(['lions', 'tigers', 'bears']);
+// console.log(sentence) // 'lions, tigers, and bears!'
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Functions
+
+Take a look at main.js
+
+Write a function to accomplish each task.
+
+Each task has an example of how it would look to use for example:
+
+If the task says
+
+> Write a function called 'greeting' that returns "Good Morning!"
+
+and the example usage says 
+
+```
+var message = greeting();
+console.log(message) // "Good Morning!"
+```
+
+When you run that code with your function it should log "Good Morning!"
+
+# Bonus
+
+Append each answer to the DOM based on the ids in index.html
